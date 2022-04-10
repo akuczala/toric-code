8200f28710f5590c14a9e5ce229687411b86f560
Alex Kuczala <kookzala@gmail.com>
2022-04-10 09:48:05 -0400

    methods for building hamiltonian from terms

--- scratch.py
+++ scratch.py
@@ -2,11 +2,13 @@ import itertools
 from typing import Dict, List
 
 import scipy.linalg as lin
+import numpy as np
 
 from toriccode.env import *
 from toriccode.plot_utils import *
-from toriccode.toric_terms import Star, Plaquette
-from toriccode.utils import *
+from toriccode.toric_terms import Star, Plaquette, Term
+from toriccode.utils import tensor_product
+from toriccode.operators import Operator, PauliOperator
 
 GRID_POINT_CLASS = make_grid_point_torus(2, 2)
 
@@ -28,18 +30,15 @@ def test_stuff():
 def hamiltonian_plots(h):
     plot_bwr(h)
     eigs, vecs = lin.eigh(h)
-    plot_bwr(eigs.reshape(1, -1))
+    plt.plot(eigs, marker='o');
+    plt.show()
     plot_bwr(vecs);
 
 
-def build_H(qubit_terms: Dict[Link[None], List[Link[Operator]]]):
-    pass
-    # todo build hamiltonian from qubits and terms
-    # return sum(
-    #     tensor_product(
-    #
-    #     for qubit, term_link in qubit_terms.items())
-    # )
+def build_H(qubit_terms: List[List[Operator]]) -> np.ndarray:
+    tensor_prod: np.ndarray = sum(tensor_product([op.matrix for op in term]) for term in qubit_terms)  # type: ignore
+    dim = 2 ** len(qubit_terms[0])
+    return tensor_prod.reshape(dim, dim)
 
 
 def test_dependent_types():
@@ -49,12 +48,19 @@ def test_dependent_types():
     gp2 = GP2.zero
     pass
 
-def map_operator_link(link: Link[Operator], qubit: Link[None]):
-    return link if qubit.same_link_position(link) else Link.with_new_content(PauliOperator.I)
 
-# todo sort this out. i'm tired
-def pad_term(term: Term, qubits: List[Link[None]]) -> GenericTerm:
-    return GenericTerm([map_operator_link(link) for link in term.links]) for qubit in qubits])
+def get_qubit_operators(term: Term, qubit_to_index_map) -> Dict[int, Operator]:
+    # todo assert uniqueness on links in Term somewhere
+    return {qubit_to_index_map[link.with_new_content(None)]: link.operator for link in term.links}
+
+
+def identity_pad_operators(n_qubits, qubit_index_operator_map: Dict[int, Operator]) -> Dict[int, Operator]:
+    return {i: qubit_index_operator_map.get(i, PauliOperator.I) for i in range(n_qubits)}
+
+
+def operator_map_to_list(operator_map: Dict[int, Operator]) -> List[Operator]:
+    return [operator_map[i] for i in range(len(operator_map))]
+
 
 def test_stuff2():
     local_terms: List[Term] = (
@@ -64,10 +70,11 @@ def test_stuff2():
     qubits: List[Link[None]] = list(set(
         link.with_new_content(None) for term in local_terms for link in term.links
     ))
-    qubit_terms = {
-        qubit: [link for term in local_terms for link in term.links if link.same_link_position(qubit)]
-        for qubit in qubits
-    }
+    qubit_to_index_map = {qubit: i for i, qubit in enumerate(qubits)}
+    qubit_terms = [
+        operator_map_to_list(identity_pad_operators(len(qubits), get_qubit_operators(term, qubit_to_index_map)))
+        for term in local_terms
+    ]
 
     plot_kwargs = {
         Star: dict(c='red', linestyle='--'),
